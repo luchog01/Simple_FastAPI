171c432fe4c4116d73adbc342d709835ef3413f4
Luciano Garbarino <lucigar01@gmail.com>
2024-05-10 13:35:16 -0300

    remove static

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from time import time
 from fastapi import FastAPI, __version__
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 html = f"""
 <!DOCTYPE html>
 <html>
     <head>
         <title>FastAPI on Vercel</title>
-        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
     </head>
     <body>
         <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
