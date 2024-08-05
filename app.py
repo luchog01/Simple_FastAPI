3906d0c5956670abd468b1dec3afcc55c58dba6f
Luciano Garbarino <lucigar01@gmail.com>
2024-08-05 00:21:25 -0300

    file test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -9,6 +9,16 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/test")
+async def test(file: UploadFile):
+    text = await file.read()
+    return {
+        "filename": file.filename,
+        "content": text.decode("utf-8"),
+        "result": "Melanoma"
+    }
+
 
 if __name__ == "__main__":
     import uvicorn
